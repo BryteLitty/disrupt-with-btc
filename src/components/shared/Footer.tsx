2841b22443f890074c85ad09f95d76c0e78d4538
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-12 13:35:40 +0000

    tweaked code

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='hidden md:block md:bg-[#000] md:py-5 lg:bg-dark lg:py-10 '>
+    <div className='hidden md:block bg-dark py-5 lg:py-10'>
       <div className='mx-10'>
-      <div className='flex flex-row items-center justify-between'>
-        <h1 className='text-light font-pop'>&copy; {''} 2025-Copyright</h1>
-      </div>
+        <div className='flex flex-row items-center justify-between'>
+          <h1 className='text-light font-pop'>&copy; 2025 — Built for real ones worldwide</h1>
+
+          <div className='font-pop text-light text-sm italic tracking-wide'>
+            no borders, no limits.
+          </div>
+        </div>
       </div>
 
-      <div className='mt-5 bg-custom-gradient h-5  mx-10'/>
- </div>
+      <div className='mt-5 bg-custom-gradient h-5 mx-10 rounded-full' />
+    </div>
   )
 }
 
